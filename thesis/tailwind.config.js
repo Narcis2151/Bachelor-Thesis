f61516e3e9c74d41b2fb0e2321c61b93a48cfd1e
Narcis <fanica.narcis10@gmail.com>
2024-04-03 20:51:40 +0300

    add spartan integration

--- thesis/tailwind.config.js
+++ thesis/tailwind.config.js
@@ -1,12 +1,13 @@
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  presets: [require('@spartan-ng/ui-core/hlm-tailwind-preset')],
+  presets: [require("@spartan-ng/ui-core/hlm-tailwind-preset")],
   content: [
-    './src/**/*.{html,ts}',
-    "./node_modules/flowbite/**/*.js"  ],
+    "./src/**/*.{html,ts}",
+    "./components/**/*.{html,ts}",
+    "./node_modules/flowbite/**/*.js",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui"), require('flowbite/plugin')],
+  plugins: [require("daisyui"), require("flowbite/plugin")],
 };
