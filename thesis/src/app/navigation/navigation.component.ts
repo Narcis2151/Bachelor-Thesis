2e564b90a6b595aa0ba4d80d1d8a0009b9905f0d
Narcis <fanica.narcis10@gmail.com>
2024-04-20 18:07:19 +0300

    working on cloding navigation dropdown when clicking outside

--- thesis/src/app/navigation/navigation.component.ts
+++ thesis/src/app/navigation/navigation.component.ts
@@ -1,22 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener } from '@angular/core';
 import { Router, NavigationStart } from '@angular/router';
 import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrl: './navigation.component.css'
+  styleUrls: ['./navigation.component.css'], // Ensure it's styleUrls, not styleUrl
 })
 export class NavbarComponent {
   dropdowns: any = {};
 
-  constructor(private router: Router) {
+  constructor(private eRef: ElementRef, private router: Router) {
     // Close all dropdowns on navigation start
-    this.router.events.pipe(
-      filter(event => event instanceof NavigationStart)
-    ).subscribe(() => {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationStart))
+      .subscribe(() => {
+        this.closeAllDropdowns();
+      });
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    if (!this.eRef.nativeElement.contains(event.target)) {
       this.closeAllDropdowns();
-    });
+    }
   }
 
   toggleDropdown(dropdown: string) {
@@ -28,4 +35,16 @@ export class NavbarComponent {
       this.dropdowns[key] = false;
     }
   }
+
+  private isClickInsideDropdown(event: MouseEvent): boolean {
+    // Adjust this query selector based on your actual dropdown button or container classes
+    const dropdownToggles = this.eRef.nativeElement.querySelectorAll('.dropdown-toggle');
+    for (const toggle of dropdownToggles) {
+      if (toggle.contains(event.target)) {
+        return true;
+      }
+    }
+    return false;
+  }
+  
 }
