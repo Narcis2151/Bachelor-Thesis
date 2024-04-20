af6816f6e6934b9a1e14ac013ea7ed65b8164137
Narcis <fanica.narcis10@gmail.com>
2024-04-20 17:29:36 +0300

    working on transactions page

--- thesis/src/app/transactions/cash-transactions-list/cash-transactions-list.component.ts
+++ thesis/src/app/transactions/cash-transactions-list/cash-transactions-list.component.ts
@@ -1,5 +1,3 @@
-import { SelectionModel } from '@angular/cdk/collections';
-import { DecimalPipe, TitleCasePipe } from '@angular/common';
 import {
   Component,
   TrackByFunction,
@@ -7,138 +5,141 @@ import {
   effect,
   signal,
 } from '@angular/core';
+import { SelectionModel } from '@angular/cdk/collections';
+import { debounceTime, map } from 'rxjs';
 import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 import { PaginatorState, useBrnColumnManager } from '@spartan-ng/ui-table-brain';
 
-import { debounceTime, map } from 'rxjs';
+import Currency from '../../../../shared/account-currency';
 
-export type Payment = {
+export type CashTransaction = {
   id: string;
   amount: number;
+  currency?: Currency;
   status: 'pending' | 'processing' | 'success' | 'failed';
-  email: string;
+  beneficiary: string;
 };
 
-const PAYMENT_DATA: Payment[] = [
+const CashTransaction_DATA: CashTransaction[] = [
   {
     id: 'm5gr84i9',
     amount: 316,
     status: 'success',
-    email: 'ken99@yahoo.com',
+    beneficiary: 'ken99@yahoo.com',
   },
   {
     id: '3u1reuv4',
     amount: 242,
     status: 'success',
-    email: 'Abe45@gmail.com',
+    beneficiary: 'Abe45@gmail.com',
   },
   {
     id: 'derv1ws0',
     amount: 837,
     status: 'processing',
-    email: 'Monserrat44@gmail.com',
+    beneficiary: 'Monserrat44@gmail.com',
   },
   {
     id: '5kma53ae',
     amount: 874,
     status: 'success',
-    email: 'Silas22@gmail.com',
+    beneficiary: 'Silas22@gmail.com',
   },
   {
     id: 'bhqecj4p',
     amount: 721,
     status: 'failed',
-    email: 'carmella@hotmail.com',
+    beneficiary: 'carmella@hotmail.com',
   },
   {
     id: 'p0r8sd2f',
     amount: 123,
     status: 'failed',
-    email: 'john.doe@example.com',
+    beneficiary: 'john.doe@example.com',
   },
   {
     id: '8uyv3n1x',
     amount: 589,
     status: 'processing',
-    email: 'emma.smith@gmail.com',
+    beneficiary: 'emma.smith@gmail.com',
   },
   {
     id: '2zqo6ptr',
     amount: 456,
     status: 'success',
-    email: 'jackson78@hotmail.com',
+    beneficiary: 'jackson78@hotmail.com',
   },
   {
     id: 'l7we9a3m',
     amount: 632,
     status: 'success',
-    email: 'grace_22@yahoo.com',
+    beneficiary: 'grace_22@yahoo.com',
   },
   {
     id: 'o9p2v3qk',
     amount: 987,
     status: 'failed',
-    email: 'robert.adams@gmail.com',
+    beneficiary: 'robert.adams@gmail.com',
   },
   {
     id: 'q1o8r7mz',
     amount: 321,
     status: 'processing',
-    email: 'alexander34@gmail.com',
+    beneficiary: 'alexander34@gmail.com',
   },
   {
     id: 'i5n3s0tv',
     amount: 555,
     status: 'failed',
-    email: 'olivia_morris@hotmail.com',
+    beneficiary: 'olivia_morris@hotmail.com',
   },
   {
     id: '3xr7s2nl',
     amount: 789,
     status: 'success',
-    email: 'michael_cole@yahoo.com',
+    beneficiary: 'michael_cole@yahoo.com',
   },
   {
     id: 'u9v2p1qy',
     amount: 234,
     status: 'success',
-    email: 'lily.jones@gmail.com',
+    beneficiary: 'lily.jones@gmail.com',
   },
   {
     id: 'b4q0e1cp',
     amount: 876,
     status: 'failed',
-    email: 'ryan_14@hotmail.com',
+    beneficiary: 'ryan_14@hotmail.com',
   },
   {
     id: 's1z8m7op',
     amount: 456,
     status: 'success',
-    email: 'sophia.green@gmail.com',
+    beneficiary: 'sophia.green@gmail.com',
   },
   {
     id: 'n5a3v0lt',
     amount: 987,
     status: 'failed',
-    email: 'david.miller@yahoo.com',
+    beneficiary: 'david.miller@yahoo.com',
   },
   {
     id: '2qr7v9sm',
     amount: 654,
     status: 'processing',
-    email: 'emma_jones@hotmail.com',
+    beneficiary: 'emma_jones@hotmail.com',
   },
   {
     id: 'y9b2h8qq',
     amount: 789,
     status: 'success',
-    email: 'jacob_89@gmail.com',
+    beneficiary: 'jacob_89@gmail.com',
   },
   {
     id: 'c4a0r1xp',
     amount: 123,
     status: 'failed',
-    email: 'samantha.richards@yahoo.com',
+    beneficiary: 'samantha.richards@yahoo.com',
   },
 ];
 
@@ -149,7 +150,7 @@ const PAYMENT_DATA: Payment[] = [
 })
 export class CashTransactionsListComponent {
   protected readonly _rawFilterInput = signal('');
-  protected readonly _emailFilter = signal('');
+  protected readonly _transactionsFilter = signal('');
   private readonly _debouncedFilter = toSignal(
     toObservable(this._rawFilterInput).pipe(debounceTime(300))
   );
@@ -158,9 +159,9 @@ export class CashTransactionsListComponent {
   protected readonly _availablePageSizes = [5, 10, 20, 10000];
   protected readonly _pageSize = signal(this._availablePageSizes[0]);
 
-  private readonly _selectionModel = new SelectionModel<Payment>(true);
-  protected readonly _isPaymentSelected = (payment: Payment) =>
-    this._selectionModel.isSelected(payment);
+  private readonly _selectionModel = new SelectionModel<CashTransaction>(true);
+  protected readonly _isCashTransactionSelected = (CashTransaction: CashTransaction) =>
+    this._selectionModel.isSelected(CashTransaction);
   protected readonly _selected = toSignal(
     this._selectionModel.changed.pipe(map((change) => change.source.selected)),
     {
@@ -170,58 +171,57 @@ export class CashTransactionsListComponent {
 
   protected readonly _brnColumnManager = useBrnColumnManager({
     status: { visible: true, label: 'Status' },
-    email: { visible: true, label: 'Email' },
+    beneficiary: { visible: true, label: 'beneficiary' },
     amount: { visible: true, label: 'Amount ($)' },
   });
   protected readonly _allDisplayedColumns = computed(() => [
-    'select',
     ...this._brnColumnManager.displayedColumns(),
     'actions',
   ]);
 
-  private readonly _payments = signal(PAYMENT_DATA);
-  private readonly _filteredPayments = computed(() => {
-    const emailFilter = this._emailFilter()?.trim()?.toLowerCase();
-    if (emailFilter && emailFilter.length > 0) {
-      return this._payments().filter((u) =>
-        u.email.toLowerCase().includes(emailFilter)
+  private readonly _CashTransactions = signal(CashTransaction_DATA);
+  private readonly _filteredCashTransactions = computed(() => {
+    const beneficiaryFilter = this._transactionsFilter()?.trim()?.toLowerCase();
+    if (beneficiaryFilter && beneficiaryFilter.length > 0) {
+      return this._CashTransactions().filter((u) =>
+        u.beneficiary.toLowerCase().includes(beneficiaryFilter)
       );
     }
-    return this._payments();
+    return this._CashTransactions();
   });
-  private readonly _emailSort = signal<'ASC' | 'DESC' | null>(null);
-  protected readonly _filteredSortedPaginatedPayments = computed(() => {
-    const sort = this._emailSort();
+  private readonly _beneficiarySort = signal<'ASC' | 'DESC' | null>(null);
+  protected readonly _filteredSortedPaginatedCashTransactions = computed(() => {
+    const sort = this._beneficiarySort();
     const start = this._displayedIndices().start;
     const end = this._displayedIndices().end + 1;
-    const payments = this._filteredPayments();
+    const CashTransactions = this._filteredCashTransactions();
     if (!sort) {
-      return payments.slice(start, end);
+      return CashTransactions.slice(start, end);
     }
-    return [...payments]
+    return [...CashTransactions]
       .sort(
-        (p1, p2) => (sort === 'ASC' ? 1 : -1) * p1.email.localeCompare(p2.email)
+        (p1, p2) => (sort === 'ASC' ? 1 : -1) * p1.beneficiary.localeCompare(p2.beneficiary)
       )
       .slice(start, end);
   });
-  protected readonly _allFilteredPaginatedPaymentsSelected = computed(() =>
-    this._filteredSortedPaginatedPayments().every((payment) =>
-      this._selected().includes(payment)
+  protected readonly _allFilteredPaginatedCashTransactionsSelected = computed(() =>
+    this._filteredSortedPaginatedCashTransactions().every((CashTransaction) =>
+      this._selected().includes(CashTransaction)
     )
   );
   protected readonly _checkboxState = computed(() => {
     const noneSelected = this._selected().length === 0;
     const allSelectedOrIndeterminate =
-      this._allFilteredPaginatedPaymentsSelected() ? true : 'indeterminate';
+      this._allFilteredPaginatedCashTransactionsSelected() ? true : 'indeterminate';
     return noneSelected ? false : allSelectedOrIndeterminate;
   });
 
-  protected readonly _trackBy: TrackByFunction<Payment> = (
+  protected readonly _trackBy: TrackByFunction<CashTransaction> = (
     _: number,
-    p: Payment
+    p: CashTransaction
   ) => p.id;
   protected readonly _totalElements = computed(
-    () => this._filteredPayments().length
+    () => this._filteredCashTransactions().length
   );
   protected readonly _onStateChange = ({
     startIndex,
@@ -232,32 +232,32 @@ export class CashTransactionsListComponent {
   constructor() {
     // needed to sync the debounced filter to the name filter, but being able to override the
     // filter when loading new users without debounce
-    effect(() => this._emailFilter.set(this._debouncedFilter() ?? ''), {
+    effect(() => this._transactionsFilter.set(this._debouncedFilter() ?? ''), {
       allowSignalWrites: true,
     });
   }
 
-  protected togglePayment(payment: Payment) {
-    this._selectionModel.toggle(payment);
+  protected toggleCashTransaction(CashTransaction: CashTransaction) {
+    this._selectionModel.toggle(CashTransaction);
   }
 
   protected handleHeaderCheckboxChange() {
     const previousCbState = this._checkboxState();
     if (previousCbState === 'indeterminate' || !previousCbState) {
-      this._selectionModel.select(...this._filteredSortedPaginatedPayments());
+      this._selectionModel.select(...this._filteredSortedPaginatedCashTransactions());
     } else {
-      this._selectionModel.deselect(...this._filteredSortedPaginatedPayments());
+      this._selectionModel.deselect(...this._filteredSortedPaginatedCashTransactions());
     }
   }
 
-  protected handleEmailSortChange() {
-    const sort = this._emailSort();
+  protected handleBeneficiarySortChange() {
+    const sort = this._beneficiarySort();
     if (sort === 'ASC') {
-      this._emailSort.set('DESC');
+      this._beneficiarySort.set('DESC');
     } else if (sort === 'DESC') {
-      this._emailSort.set(null);
+      this._beneficiarySort.set(null);
     } else {
-      this._emailSort.set('ASC');
+      this._beneficiarySort.set('ASC');
     }
   }
 }
