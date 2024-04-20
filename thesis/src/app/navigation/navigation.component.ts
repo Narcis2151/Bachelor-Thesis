52f8ba0a617110bbd2d3052eacdb7b5b15ba4eb0
Narcis <fanica.narcis10@gmail.com>
2024-04-20 17:55:40 +0300

    update navigation with router links and active classes

--- thesis/src/app/navigation/navigation.component.ts
+++ thesis/src/app/navigation/navigation.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { Router, NavigationStart } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrl: './navigation.component.scss'
+  styleUrl: './navigation.component.css'
 })
-export class NavigationComponent {
+export class NavbarComponent {
+  dropdowns: any = {};
 
+  constructor(private router: Router) {
+    // Close all dropdowns on navigation start
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationStart)
+    ).subscribe(() => {
+      this.closeAllDropdowns();
+    });
+  }
+
+  toggleDropdown(dropdown: string) {
+    this.dropdowns[dropdown] = !this.dropdowns[dropdown];
+  }
+
+  closeAllDropdowns() {
+    for (const key in this.dropdowns) {
+      this.dropdowns[key] = false;
+    }
+  }
 }
